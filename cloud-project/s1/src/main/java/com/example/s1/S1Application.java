d52ddd2f78cebc7ecececab58ab1aa6640e9fb81
zhangfx <zhangfuxing1010@163.com>
2023-01-05 17:32:33 +0800

    添加网关服务

--- cloud-project/s1/src/main/java/com/example/s1/S1Application.java
+++ cloud-project/s1/src/main/java/com/example/s1/S1Application.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients
 public class S1Application {
 	
 	public static void main(String[] args) {
--- cloud-project/s2/src/main/java/com/example/S2Application.java
+++ cloud-project/s2/src/main/java/com/example/S2Application.java
@@ -1,10 +1,12 @@
 package com.example;
 
+import com.example.http.S1Service;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -16,18 +18,22 @@ import javax.annotation.Resource;
 @RestController
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients
 public class S2Application {
 	public static void main(String[] args) {
 		SpringApplication.run(S2Application.class,args);
 	}
 	@Resource
 	RestTemplate template;
+	
+	@Resource
+	S1Service s1;
+	
 	@Value("${config.info}")
 	private String info;
 	
-	@GetMapping("/get")
+	@GetMapping("/info")
 	public String get() {
-		String s = template.getForObject("http://service1/info", String.class);
-		return "service1-data=" + s + " | service2-data=" + info;
+		return "service1-data=" + s1.getInfo() + " | service2-data=" + info;
 	}
 }
