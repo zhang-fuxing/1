a4c620d1e25c8190251a6eb47017baee20e4c6af
zhangfx <zhangfuxing1010@163.com>
2023-02-02 15:20:19 +0800

    代理模式实现

---
+++ 设计模式/src/main/java/代理/dynamicproxy/Main.java
@@ -0,0 +1,18 @@
+package 代理.dynamicproxy;
+
+import 代理.dynamicproxy.fun.UserService;
+import 代理.dynamicproxy.impl.UserServiceImpl;
+import 代理.dynamicproxy.proxy.ProxyFactory;
+
+/**
+ * @author zfx
+ * @date 2022-07-24 20:47
+ */
+public class Main {
+	public static void main(String[] args) {
+		// dynamic proxy
+		UserServiceImpl userService = new UserServiceImpl();
+		UserService proxyObject = new ProxyFactory<UserService>(userService).getProxyObject();
+		proxyObject.tt();
+	}
+}
